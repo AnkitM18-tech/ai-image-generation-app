204fb1af29cf3264d1a6d25c841b2bc634f85b18
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2023-02-15 02:59:46 +0530

    rectified cloud name

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,7 +8,7 @@ dotenv.config();
 const router = express.Router();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
